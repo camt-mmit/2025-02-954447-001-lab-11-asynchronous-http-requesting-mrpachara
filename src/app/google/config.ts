d6bcaa29474c0ae6f4b61745ce2d7fd736def8da
Pachara Tinamas <mrpachara@gmail.com>
2026-03-01 04:35:21 +0700

    fix: wrong client_id and redirec_url

--- src/app/google/config.ts
+++ src/app/google/config.ts
@@ -1,12 +1,13 @@
+import { isDevMode } from '@angular/core';
 import { OauthClientConfiguration } from './types/services';
 
 export const googleOauthConfig: OauthClientConfiguration = {
   name: 'google',
-  // id: '209689905225-dj1bo29m0c7or5926cv4bb1nu5aru0cv.apps.googleusercontent.com',
-  // secret: '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',
-  id: '209689905225-o4li7ageadhnatgj9q1hpo7o0a0hpbsd.apps.googleusercontent.com',
-  secret: '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',
+  id: '209689905225-dj1bo29m0c7or5926cv4bb1nu5aru0cv.apps.googleusercontent.com',
+  secret: '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',
   tokenUrl: 'https://oauth2.googleapis.com/token',
   authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
-  redirectUrl: 'http://localhost:4200/google/authorization',
+  redirectUrl: isDevMode()
+    ? 'http://localhost:4200/google/authorization'
+    : 'https://camt-mmit.github.io/2025-02-954447-001-lab-11-asynchronous-http-requesting-mrpachara/google/authorization',
 };
